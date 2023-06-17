9deca27ffa34be31519fe8ca399dd9a2b999dea0
David Araujo <david.araujo@grupobusiness.it>
2023-06-17 00:23:54 -0500

    Feat: User managment working

--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { ApiHandlerService } from 'src/services/api-handler.service';
+import { ToastrService } from 'ngx-toastr';
 
 export interface UserListResponse {
   code: number;
@@ -20,8 +21,8 @@ export interface UserData {
   role: string;
   token: string | null;
   createDate: string;
-  status:string;
-  editMode:boolean;
+  status: string;
+  editMode: boolean;
 }
 
 @Component({
@@ -30,35 +31,47 @@ export interface UserData {
   styleUrls: ['./user-management.component.css'],
 })
 export class UserManagementComponent implements OnInit {
-  
-  constructor(private http: HttpClient, private apiHandler: ApiHandlerService) {}
+  constructor(
+    private http: HttpClient,
+    private apiHandler: ApiHandlerService,
+    private toastr: ToastrService
+  ) {}
 
   globalUrl = this.apiHandler.apiUrl;
   users: UserData[] = [];
 
-  
   ngOnInit(): void {
     this.fetchUsers();
   }
 
-
   fetchUsers(): void {
-    this.http.get<UserListResponse>(this.globalUrl+'/get-getallusers')
+    this.http
+      .get<UserListResponse>(this.globalUrl + '/get-getallusers')
       .subscribe(
         (response: UserListResponse) => {
           this.users = response.data.map((user: UserData) => ({
             ...user,
-            editMode: false // Add the "editMode" property with a default value of false
+            editMode: false, // Add the "editMode" property with a default value of false
           }));
-          console.log(this.users); 
+          console.log(this.users);
         },
         (error) => {
           console.error('Error fetching users:', error);
         }
       );
   }
-  
-  getBackgroundColor(status: string): string {
+
+  getIconStatusBackgroundColor(status: string): string {
+    if (status === 'Active') {
+      return 'rgba(180, 2, 2, 0.418)';
+    } else if (status === 'Inactive' || status.trim() == '') {
+      return 'rgba(2, 180, 2, 0.418)';
+    } else {
+      return '';
+    }
+  }
+
+  getStatusBackgroundColor(status: string): string {
     if (status === 'Active') {
       return 'rgba(2, 180, 2, 0.418)';
     } else if (status === 'Inactive') {
@@ -69,32 +82,69 @@ export class UserManagementComponent implements OnInit {
   }
 
   toggleEditMode(user: any): void {
-    user.editMode = !user.editMode; 
+    user.editMode = !user.editMode;
   }
 
-}
+  changeUserStatus(user: UserData): void {
+    var status = false;
 
-/*
-  editUser(user: any) {
-    // Call the API to update the user data
-    
+    if (user.status.trim() == '' || user.status == 'Inactive') {
+      status = true;
+    }
+    if (user.status == 'Active') {
+      status = false;
+    }
+
     const formData = new FormData();
-    formData.append('id', user.id);
+    formData.append('userId', user.id.toString());
+    formData.append('isUserActive', status.toString());
+    this.http
+      .post(this.apiHandler.apiUrl + '/post-changestatus', formData)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          this.fetchUsers();
+          if(status){
+            console.log('Usuario activado!');
+            this.toastr.success('Usuario activado!');
+          }
+          if(status){
+            console.log('Usuario desactivado!');
+            this.toastr.success('Usuario desactivado!');
+          }
+          
+          
+        },
+        (error) => {
+          console.log('Error updating status:', error);
+          this.toastr.error('Error updating status.');
+        }
+      );
+    
+  }
+
+  editUser(user: UserData): void {
+    this.toggleEditMode(user);
+
+    const formData = new FormData();
+    formData.append('id', user.id.toString());
     formData.append('firstname', user.firstname);
     formData.append('lastname', user.lastname);
     formData.append('email', user.email);
     formData.append('role', user.role);
-  
-    // Make the HTTP request using Angular's HttpClient
-    this.http.post(this.apiHander.apiUrl +'/post-updateuser', formData).subscribe(
-      (response) => {
-        console.log(response);
-        console.log('User updated successfully');
-        // Update the user data in the table (optional)
-      },
-      (error) => {
-        console.log('Error updating user:', error);
-      }
-    );
+
+    this.http
+      .post(this.apiHandler.apiUrl + '/post-updateuser', formData)
+      .subscribe(
+        (response) => {
+          console.log(response);
+          console.log('User updated successfully');
+          this.toastr.success('User updated succesfully!');
+        },
+        (error) => {
+          console.log('Error updating user:', error);
+          this.toastr.error('Error updating user.');
+        }
+      );
   }
-  */
+}
