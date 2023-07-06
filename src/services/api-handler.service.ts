42fff50490fb79f79659ddf4cd5d50b70b96cffb
MADMVX <dskato0603@gmail.com>
2023-07-06 16:28:56 -0500

    fix: host

--- src/services/api-handler.service.ts
+++ src/services/api-handler.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ApiHandlerService {
   
-  public apiUrl: string = 'https://localhost:7016';
+  public apiUrl: string = 'https://arappbackend.onrender.com';
   
   
 }
